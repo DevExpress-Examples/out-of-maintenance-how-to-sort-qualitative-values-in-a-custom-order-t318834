d7a4bce932d2dd8ef239f6c052aad10d2ea279ed
DevExpressExampleBot <fakeEmail>
2022-12-18 20:47:32 +0400

    Source auto update [skip ci]

--- VB/CustomSortOrder/NumberComparer.vb
+++ VB/CustomSortOrder/NumberComparer.vb
@@ -1,38 +1,36 @@
-﻿#Region "#ComparerImpl"
-Imports System
+#Region "#ComparerImpl"
 Imports System.Collections
 
 Namespace CustomSortOrder
-	Friend Class NumberComparer
-		Implements IComparer
 
-		Public Function Compare(ByVal x As Object, ByVal y As Object) As Integer Implements IComparer.Compare
-			Dim iX As Integer = NumberConverter.ToInt(x)
-			Dim iY As Integer = NumberConverter.ToInt(y)
-			Return iX - iY
-		End Function
-	End Class
+    Friend Class NumberComparer
+        Implements IComparer
 
-	Friend Class NumberConverter
-		Public Shared Function ToInt(ByVal o As Object) As Integer
-			Dim stringNumber As String = TryCast(o, String)
-			Dim number As Integer = 0
-			If stringNumber Is Nothing Then
-				Return -1
-			End If
-			If Int32.TryParse(stringNumber, number) Then
-				Return number
-			End If
-			Select Case stringNumber.ToLower()
-				Case "one"
-					Return 1
-				Case "two"
-					Return 2
-				Case "three"
-					Return 3
-			End Select
-			Return number
-		End Function
-	End Class
+        Public Function Compare(ByVal x As Object, ByVal y As Object) As Integer Implements IComparer.Compare
+            Dim iX As Integer = NumberConverter.ToInt(x)
+            Dim iY As Integer = NumberConverter.ToInt(y)
+            Return iX - iY
+        End Function
+    End Class
+
+    Friend Class NumberConverter
+
+        Public Shared Function ToInt(ByVal o As Object) As Integer
+            Dim stringNumber As String = TryCast(o, String)
+            Dim number As Integer = 0
+            If Equals(stringNumber, Nothing) Then Return -1
+            If Integer.TryParse(stringNumber, number) Then Return number
+            Select Case stringNumber.ToLower()
+                Case "one"
+                    Return 1
+                Case "two"
+                    Return 2
+                Case "three"
+                    Return 3
+            End Select
+
+            Return number
+        End Function
+    End Class
 End Namespace
-#End Region ' #ComparerImpl
+#End Region  ' #ComparerImpl
